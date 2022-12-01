55dce416e366be17a3f30089d20eb0224320e6ce
ggmvic <113037468+ggmvic@users.noreply.github.com>
2022-12-01 19:57:37 +0100

    Update script_practicaSQL_mvgg.sql

--- script_practicaSQL_mvgg.sql
+++ script_practicaSQL_mvgg.sql
@@ -269,8 +269,3 @@ insert into practicaSQL_mvgg.REVISIONS (dt_revision, idvehicle, kms, price, idcu
 insert into practicaSQL_mvgg.REVISIONS (dt_revision, idvehicle, kms, price, idcurrency, description) values('2021-12-01','029','11558','200','01','Segunda Revisión');
 insert into practicaSQL_mvgg.REVISIONS (dt_revision, idvehicle, kms, price, idcurrency, description) values('2021-12-01','030','24317','200','01','Segunda Revisión');
 
-
-
-select a.idvehicle, b.name as make_car, c.name as Model, d.name as Group, a.number_plate, f.name as Color, a.kms , a.dt_bought, e.name as Insurance_Comp , a.num_policy  
-from practicaSQL_mvgg.vehicles a, practicaSQL_mvgg.make_cars b, practicaSQL_mvgg.models c, practicaSQL_mvgg.group d, practicaSQL_mvgg.insurance_company e, practicaSQL_mvgg.color f
-where a.idmake_car = b.idmake_car and a.idmodel = c.idmodel and a.idgroup = d.idgroup and a.idcolor = f.idcolor and a.idcompany = e.idcompany ;
